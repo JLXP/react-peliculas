af05125decd48d256c4d0ee4f7e977392ff30713
JavierXool <javier.xool@outlook.com>
2026-06-07 16:04:55 -0600

    Expresiones jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 function App() {
 
+  const subtitulo = 'Este es otro subtitulo';
+
+  const duplicar = (valor:number)=> valor * 2;
+  const imagenURL = '/vite.svg';
 
   return (
     <>
       <h1>Hola Mundo</h1>
-      <h3>Esta es mi pagina</h3>
+      <h3>{subtitulo.toUpperCase()}</h3>
+      <h4>El doble de 3 es {duplicar(3)}</h4>
+      <img src={imagenURL} alt="logo vista"/>
     </>
     
   )
