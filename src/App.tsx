ac46e732fd32e0a97faf0ba739f3b268a6b50c97
JavierXool <javier.xool@outlook.com>
2026-06-08 19:27:16 -0600

    css en react

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import styles from './App.module.css';
+
+
 function App() {
 
   const subtitulo = 'Este es otro subtitulo';
@@ -5,11 +8,22 @@ function App() {
   const duplicar = (valor:number)=> valor * 2;
   const imagenURL = '/vite.svg';
 
+  const cuadradoRojo = {
+    backgroundColor: 'red',
+    width: '50px',
+    height:'50px',
+    marginLeft: '1rem'
+  }
+
   return (
     <>
-      <h1>Hola Mundo</h1>
-      <h3>{subtitulo.toUpperCase()}</h3>
+      <h1 className="rojo">Hola Mundo</h1>
+      <div style={cuadradoRojo}></div>
+
+      <h3 className={styles.color}>{subtitulo.toUpperCase()}</h3>
       <h4>El doble de 3 es {duplicar(3)}</h4>
+
+      <div style={cuadradoRojo}></div>
       <img src={imagenURL} alt="logo vista"/>
     </>
     
