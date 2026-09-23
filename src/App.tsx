d586b7461c7bc6919f24c87c64670112bfd742af
JavierXool <javier.xool@outlook.com>
2026-09-22 22:23:04 -0600

    Componente Hijo: Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Cabecera from "./Cabecera";
+import MostrarTexto from "./MostrarTexto";
 
 function App() {
 
@@ -20,14 +22,12 @@ function App() {
 
   return (
     <>
-      <h1 className="rojo">Hola Mundo</h1>
+    <Cabecera/>
       <button onClick={manejarClick}>Clickeame</button>
       <div>
         <input onKeyUp={(e)=> manejarKeyUp(e)}/>
       </div>
-      <div>
-        <p>Haz escrito: {texto}</p>
-      </div>
+    <MostrarTexto texto={texto} />
     </>
     
   )
