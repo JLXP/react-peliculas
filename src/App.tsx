b4292bf84b942bdcf3ccf46ce1e7b9f955e8a127
JavierXool <javier.xool@outlook.com>
2026-09-24 21:37:15 -0600

    contenido dinamico parte 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
-import ContenidoDinamico from "./ContenidoDinamico";
+import ContenidoDinamicoIf from "./ContenidoDinamicoIf";
 
 export default function App() {
-
-  const [mostrar, setMostrar] = useState(false); 
-    
+  const [calificacion, setCalificacion] = useState<number | null>(null);
 
   return (
     <>
-        <input type="checkbox" onChange={e=> setMostrar(e.target.checked)}/>
-        <label>Mostrar el mensaje oculto</label>
-        <ContenidoDinamico mostrarContenido={ }/>
+      <label>Ingrese la calificacion:</label>
+      <input
+        type="number"
+        onChange={(e) => setCalificacion(Number(e.target.value))}
+      />
+
+      {calificacion ? (
+        <ContenidoDinamicoIf calificacion={calificacion} />
+      ) : undefined}
     </>
-    
-  )
+  );
 }
-
-
-
