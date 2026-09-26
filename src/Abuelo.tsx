3ccd5dddd09770e111268367be5d0bf2b7158186
JavierXool <javier.xool@outlook.com>
2026-09-25 18:36:00 -0600

    useContextpart1

---
+++ src/Abuelo.tsx
@@ -0,0 +1,8 @@
+export default function Abuelo() {
+  return (
+    <>
+      <h3> Este es el componente abuelo</h3>
+      <Padre />
+    </>
+  );
+}
---
+++ src/Hijo.tsx
@@ -0,0 +1,3 @@
+export default function Hijo(){
+    return <h3>Este es el component hijo</h3>
+}
